d4d6673ef4838f8914bf77594050bb9feea42b04
brendannee <brendannee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-31 08:34:48 +0000

    Version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/old-page-reminder/trunk@200087 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- page-reminder.php
+++ page-reminder.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * @package Page Reminder
+ * @package Old Page Reminder
  * @author Brendan Nee - blinktag.com
- * @version 1
+ * @version 0.2
  */
 /*
-Plugin Name: Page Reminder
+Plugin Name: Old Page Reminder
 Plugin URI: http://blinktag.com
 Description: Displays a reminder on the admin page to update posts
 Author: Brendan Nee
-Version: 1
+Version: 0.2
 Author URI: http://blinktag.com
 */
 
@@ -22,7 +22,7 @@ define('WP_CONTENT_WEB', get_bloginfo('wpurl') . '/wp-content');
 /**
  * Web-accessible control panel page.
  */
-define('WP_CPANEL', get_bloginfo('wpurl') . '/wp-admin/options-general.php?page=page-reminder');
+define('WP_CPANEL', get_bloginfo('wpurl') . '/wp-admin/options-general.php?page=old-page-reminder');
 
 /**
  * Set absolute plugin directory.
@@ -46,11 +46,88 @@ define('PAGE_REMINDER_CACHEDIR', dirname(dirname(PAGE_REMINDER_PLUGINDIR)) . '/c
 
 
 ini_set('display_errors', 1); 
+
+
+function oldpage_init($days=90)
+{
+
+    add_option('oldpage-days', 90);
+
+	add_action('wp_dashboard_setup', 'wp_dashboard_add_page_reminder' );
+
+	// Hook for adding admin menus
+	add_action('admin_menu', 'mt_add_pages');
+}
+
+// action function for above hook
+function mt_add_pages() {
+    // Add a new submenu under Options:
+    add_options_page('Page Reminder', 'Page Reminder', 'administrator', 'old-page-reminder', 'mt_options_page');
+}
+
+// mt_options_page() displays the page content for the Test Options submenu
+function mt_options_page() {
 	
+ // variables for the field and option names 
+    $opt_name = 'oldpage-days';
+    $hidden_field_name = 'mt_submit_hidden';
+    $data_field_name = 'oldpage-days';
+
+    // Read in existing option value from database
+    $opt_val = get_option( $opt_name );
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $opt_val = $_POST[ $data_field_name ];
+
+        // Save the posted value in the database
+        update_option( $opt_name, $opt_val );
+
+        // Put an options updated message on the screen
+
+?>
+	<div class="updated"><p><strong><?php _e('Options saved.', 'mt_trans_domain' ); ?></strong></p></div>
+	<?php
+
+	    }
+
+	    // Now display the options editing screen
+
+	    echo '<div class="wrap">';
+
+	    // header
+
+	    echo "<h2>" . __( 'Old Page Reminder', 'mt_trans_domain' ) . "</h2>";
+
+	    // options form
+
+	    ?>
+
+	<form name="form1" method="post" action="">
+	<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+
+	<p><?php _e("Number of Days:", 'mt_trans_domain' ); ?> 
+	<input type="text" name="<?php echo $data_field_name; ?>" value="<?php echo $opt_val; ?>" size="20"><br>
+	<?php _e("How old should posts be before they show up on the dashboard Old Page Reminder box?"); ?>
+	</p><hr />
+
+	<p class="submit">
+	<input type="submit" name="Submit" class="button-primary" value="<?php _e('Update Options', 'mt_trans_domain' ) ?>" />
+	</p>
+
+	</form>
+	</div>
+
+	<?php
+}
 		
 function wp_dashboard_page_reminder ( ) {
 	global $wpdb;
-	$pages = $wpdb->get_results("SELECT ID, post_title, post_modified FROM $wpdb->posts WHERE post_type='page' AND post_modified < DATE_SUB(NOW(),INTERVAL 90 DAY) AND ID <> '304' AND ID <> '306' ORDER BY post_modified ASC");
+	
+	$days = get_option('oldpage-days');
+	$pages = $wpdb->get_results("SELECT ID, post_title, post_modified FROM $wpdb->posts WHERE post_type='page' AND post_modified < DATE_SUB(NOW(),INTERVAL $days DAY) AND ID <> '304' AND ID <> '306' ORDER BY post_modified ASC");
 
 		
 	if ( $pages && is_array( $pages ) ) {
@@ -71,7 +148,7 @@ function wp_dashboard_page_reminder ( ) {
 	<div style="clear:both;"></div>
 <?php
 	} else {
-		_e('All pages have been updated in the last three months.');
+		_e("All pages have been updated in the last $days days.");
 	}
 }
 
@@ -102,6 +179,6 @@ function wp_dashboard_add_page_reminder() {
 	$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
 } 
 
-add_action('wp_dashboard_setup', 'wp_dashboard_add_page_reminder' );
+oldpage_init();
 
 ?>
